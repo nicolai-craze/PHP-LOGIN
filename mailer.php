17a5a3539ec1ff349d6b1c1a27d5bf9a8debb612
Nicanor wafula <sitb01-029002022@student.mmust.ac.ke>
2024-12-28 17:56:45 +0300

    Add files via upload

---
+++ mailer.php
@@ -0,0 +1,24 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require __DIR__ . "/vendor/autoload.php";
+
+$mail = new PHPMailer(true);
+
+ //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.example.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+$mail->Username = "nicanorwafula@gmail.com";
+$mail->Password = "Nicolai@2003";
+
+$mail->isHtml(true);
+
+return $mail;
